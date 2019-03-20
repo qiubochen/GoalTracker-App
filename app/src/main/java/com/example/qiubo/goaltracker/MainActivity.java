da290ec5050e083cc76edba2a37bed73a4411e69
qiubobo <q790240494@gmail.com>
2019-03-20 16:55:52 +0800

    完成了提交任务的功能

--- app/src/main/java/com/example/qiubo/goaltracker/MainActivity.java
+++ app/src/main/java/com/example/qiubo/goaltracker/MainActivity.java
@@ -10,11 +10,14 @@ import android.util.Log;
 import android.view.View;
 
 import com.example.qiubo.goaltracker.adapter.MainFragmentAdapter;
+import com.example.qiubo.goaltracker.model.DO.Event;
 import com.example.qiubo.goaltracker.view.NoScrollViewPager;
 import com.example.qiubo.goaltracker.view.impl.DataFragment;
 import com.example.qiubo.goaltracker.view.impl.EventFragment;
 import com.example.qiubo.goaltracker.view.impl.GroupFragment;
 import com.example.qiubo.goaltracker.view.impl.PersonFragment;
+
+import org.litepal.LitePal;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,62 +35,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //button=findViewById(R.id.button);
        // button.setOnClickListener(this);
         initView();
+        List<Event>eventList=LitePal.findAll(Event.class);
+        for (Event e:eventList){
+            System.out.println(e.getEvent());
+            System.out.println(e.getPlanStartTime());
+        }
+        LitePal.deleteAll(Event.class);
     }
-    void initView(){
-        tableLayout=findViewById(R.id.main_tablayout);
-        viewPager=findViewById(R.id.main_pageview);
-        viewPager.setScanScroll(false);
-        tableLayout.addTab(tableLayout.newTab().setText(PERSON));
-        tableLayout.addTab(tableLayout.newTab().setText(GROUP));
-        tableLayout.addTab(tableLayout.newTab().setText(DATA));
-        tableLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                Log.i(TAG,"onTabSelected:"+tab.getText());
-            }
+        void initView(){
+            tableLayout=findViewById(R.id.main_tablayout);
+            viewPager=findViewById(R.id.main_pageview);
+            viewPager.setScanScroll(false);
+            tableLayout.addTab(tableLayout.newTab().setText(PERSON));
+            tableLayout.addTab(tableLayout.newTab().setText(GROUP));
+            tableLayout.addTab(tableLayout.newTab().setText(DATA));
+            tableLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    Log.i(TAG,"onTabSelected:"+tab.getText());
+                }
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
 
-            }
+                }
 
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
 
-            }
-        });
-        tableLayout.setupWithViewPager(viewPager);
-        List<Fragment>fragmentList=new ArrayList<>();
-        fragmentList.add(PersonFragment.newInstance("person","test"));
-        fragmentList.add(EventFragment.newInstance("event","test"));
-        fragmentList.add(DataFragment.newInstance("data","test"));
-        fragmentList.add(GroupFragment.newInstance("group","test"));
+                }
+            });
+            tableLayout.setupWithViewPager(viewPager);
+            List<Fragment>fragmentList=new ArrayList<>();
+            fragmentList.add(PersonFragment.newInstance("person","test"));
+            fragmentList.add(EventFragment.newInstance("event","test"));
+            fragmentList.add(DataFragment.newInstance("data","test"));
+            fragmentList.add(GroupFragment.newInstance("group","test"));
 
-        List<String>titleList=new ArrayList<>();
-        titleList.add(PERSON);
-        titleList.add(EVENT);
-        titleList.add(DATA);
-        titleList.add(GROUP);
+            List<String>titleList=new ArrayList<>();
+            titleList.add(PERSON);
+            titleList.add(EVENT);
+            titleList.add(DATA);
+            titleList.add(GROUP);
 
-        MainFragmentAdapter mainFragmentAdapter =new MainFragmentAdapter(getSupportFragmentManager(),fragmentList,titleList);
-        viewPager.setAdapter(mainFragmentAdapter);
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int i, float v, int i1) {
+            MainFragmentAdapter mainFragmentAdapter =new MainFragmentAdapter(getSupportFragmentManager(),fragmentList,titleList);
+            viewPager.setOffscreenPageLimit(4);
+            viewPager.setAdapter(mainFragmentAdapter);
+            viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int i, float v, int i1) {
 
-            }
+                }
 
-            @Override
-            public void onPageSelected(int i) {
-                Log.i(TAG,"select page:"+i);
-            }
+                @Override
+                public void onPageSelected(int i) {
+                    Log.i(TAG,"select page:"+i);
+                }
 
-            @Override
-            public void onPageScrollStateChanged(int i) {
+                @Override
+                public void onPageScrollStateChanged(int i) {
 
-            }
-        });
-    }
+                }
+            });
+        }
 
     @Override
     public void onClick(View view) {
